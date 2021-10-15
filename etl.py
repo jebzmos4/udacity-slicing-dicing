1a2ece62ff768a0be655c25beeb6e083b0bb228c
Morifeoluwa Jebutu <jebzmos4@users.noreply.github.com>
2021-10-15 11:43:13 +0100

    Create etl.py

---
+++ etl.py
@@ -0,0 +1,37 @@
+import configparser
+import psycopg2
+from sql_queries import copy_table_queries, insert_table_queries
+
+
+def load_staging_tables(cur, conn):
+    for query in copy_table_queries:
+        print(query)
+        cur.execute(query)
+        conn.commit()
+
+
+def insert_tables(cur, conn):
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    print('connecting to redshift cluster')
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+    
+    print('connection successful')
+    print('loading data into staging tables')
+    load_staging_tables(cur, conn)
+    print('inserting data into tables')
+    insert_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
